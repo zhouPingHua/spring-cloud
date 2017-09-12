62b9851247fe7c55966d7086657f596d390d5556
zhoupinghua@9188.com <zhoupinghua@9188.com>
2017-09-12 14:20:30 +0800

    22

--- configclient/src/main/java/com/example/demo/service/BusRefresh.java
+++ configclient/src/main/java/com/example/demo/service/BusRefresh.java
@@ -17,21 +17,11 @@ public class BusRefresh extends AbstractHttpService {
     *
     */
     public void refresh() {
-        BusRefresh main = new BusRefresh();
         LoginContext loginContext = createLoginContext(new BasicCookieStore(), null);
-        Map<String, String> headers = loginContext.getRequestHeaders();
-        headers.put("Accept", "text/html, application/xhtml+xml, */*");
-        headers.put("Accept-Language", "zh-CN");
-        headers.put("User-Agent", "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/7.0)");
-        headers.put("Accept-Encoding", "gzip, deflate");
-        headers.put("Host", "127.0.0.1");
-        headers.put("Connection", "Keep-Alive");
-//        String url = "http://127.0.0.1:8881/bus/refresh?destination=config-client:**";
-        String url = "http://127.0.0.1:8881/bus/refresh";
-        loginContext.setEncoding("utf-8");
-        loginContext.setUri(url);
+//        String url = "http://127.0.0.1:8881/bus/refresh";
+//        loginContext.setEncoding("utf-8");
+        loginContext.setUri("http://127.0.0.1:8881/bus/refresh?destination=config-client:**");
         loginContext.setParams(null);
-        String errHtml = doPost(loginContext);
-        System.out.println(  errHtml);
+        doPost(loginContext);
     }
 }
